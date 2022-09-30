5b308a994517e1291856016ffe97742b020092fb
Pawel <skrobskipawel@gmail.com>
2022-09-30 19:23:24 +0200

    initial front

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 import './App.css';
+import Dashboard from './components/Dashboard';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App bg-dark text-white">
+      <Container className="justify-content-center p-10">
+        <Row className="justify-content-center">
+          <Col md={4}><h1>Data options</h1></Col>
+        </Row>
+        <Row className="justify-content-center">
+          <Col md={4}><Dashboard className="container" /></Col>
+        </Row>
+      </Container>
     </div>
   );
 }
---
+++ src/components/Dashboard.js
@@ -0,0 +1,32 @@
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import { useState } from 'react';
+
+const Dashboard = () => {
+
+    const [error, setError] = useState(0);
+    const [seed, setSeed] = useState(0);
+
+    return (
+        <Form className='m-3'>
+            <Form.Select aria-label="Defarault select" >
+                <option value="pl">Poland</option>
+                <option value="usa">USA</option>
+                <option value="ge">Georgia</option>
+            </Form.Select>
+            <Form.Label className='m-3'>Error value: {error}</Form.Label>
+            <Form.Range min="0" max="1" step="0.1" value={error} onChange={(e) => setError(e.target.value)} />
+            <Form.Control
+                type="number"
+                placeholder="Seed"
+                aria-label="Seed"
+                value={seed}
+                onChange={(e) => setSeed(e.target.value)}
+                aria-describedby="basic-addon1"
+            />
+            <Button variant="primary" className='m-3'>Get random data</Button>{' '}
+        </Form>
+    );
+}
+
+export default Dashboard;
